69638fce68d450d24700c6552c0ea030cb9b6e86
muckalo <grcic@gmail.com>
2017-04-28 16:39:09 +0200

    Implemented LUPowerOutages Indexer

--- tests/scrapers/power_outages/lu_power_outages_tests.py
+++ tests/scrapers/power_outages/lu_power_outages_tests.py
@@ -38,15 +38,13 @@ class LUPowerOutagesTest(unittest.TestCase):
         url = request_args.get('url')
         verify = request_args.get('verify')
         headers = request_args.get('headers')
-        params = request_args.get('params')
         data = request_args.get('data')
 
         self.assertEqual(size, 4)
 
-        self.assertEqual(url, 'http://www.outageentry.com/CustomerFacingAppJQM/ajaxShellOut.php?target=device_markers&action=get&serviceIndex=1&url=10.58.27.9%3A80')
+        self.assertEqual(url, 'http://www.outageentry.com/CustomerFacingAppJQM/ajaxShellOut.php')
         self.assertFalse(verify)
         self.assertIsNotNone(headers)
-        self.assertIsNotNone(params)
         self.assertIsNone(data)
 
     def test_get_headers(self):
@@ -67,11 +65,6 @@ class LUPowerOutagesTest(unittest.TestCase):
         self.assertIsNone(host)
         self.assertIsNone(ramire_purpose)
 
-    def test_get_data(self):
-        data = self.scraper.get_data()
-
-        self.assertIsNone(data)
-
     def test_get_max_delay(self):
         max_delay = self.scraper.get_max_delay()
 
@@ -85,16 +78,25 @@ class LUPowerOutagesTest(unittest.TestCase):
         incident_id = result['id']
         incident = result['incident']
         created_at = result['created_at']
+        start_date = result['start_date']
+        longitude = result['longitude']
+        latitude = result['latitude']
+        consumers_affected = result['consumers_affected']
 
         self.assertEqual(created_at, 1471567800)
-        self.assertEqual(incident_id, '8680ba2f26da0213cdd3fa54eda9846a')
+        self.assertEqual(incident_id, '5501cc261b79ef4535eb70bf504d6e7f')
         self.assertEqual(incident, 'Power Outage')
+        self.assertEqual(start_date, '04/24 02:31:01 pm')
+        self.assertEqual(longitude, '-84.0379808698967')
+        self.assertEqual(latitude, '36.3322327488042')
+        self.assertEqual(consumers_affected, '14')
 
     @patch('time.time', return_value=1471568199)
     def test_scrape_delay(self, mock_time):
         incidents = self.data['response']
         size = sum(1 for i in self.scraper.scrape(incidents))
-        self.assertEqual(size, 3)
+
+        self.assertEqual(size, 1)
 
     def test_parse_alert(self):
         alert = self.scraper.parse(self.data['incident'])
@@ -134,16 +136,16 @@ class LUPowerOutagesTest(unittest.TestCase):
         self.assertEqual(len(source_keys), 6)
         self.assertEqual(len(provider_keys), 5)
 
-        self.assertEqual(created_at, 1471567800.0)
-        self.assertEqual(id, '8680ba2f26da0213cdd3fa54eda9846a')
+        self.assertEqual(created_at, 1471567800)
+        self.assertEqual(id, '5501cc261b79ef4535eb70bf504d6e7f')
         self.assertEqual(text, 'Power Outage')
         self.assertEqual(source_type, TYPE_CAD_API)
 
         self.assertEqual(provider_api_host, 'www.outageentry.com')
-        self.assertEqual(provider_api_route, '/CustomerFacingAppJQM/ajaxShellOut.php?target=device_markers&action=get&serviceIndex=1&url=10.58.27.9%3A80')
+        self.assertEqual(provider_api_route, '/CustomerFacingAppJQM/ajaxShellOut.php')
         self.assertEqual(provider_api_name, 'LaFollette Utilities')
         self.assertEqual(provider_api_id, 'lu_power_outages')
-        self.assertEqual(provider_api_url, 'http://www.outageentry.com/CustomerFacingAppJQM/outage.php?clientid=LAFOLLETTE')
+        self.assertEqual(provider_api_url, 'https://www.outageentry.com/CustomerFacingAppJQM/outage.php?clientid=LAFOLLETTE')
 
     @patch('time.time', return_value=1471568199)
     def test_run(self, mock_time):
@@ -151,7 +153,7 @@ class LUPowerOutagesTest(unittest.TestCase):
         self.scraper.request = MagicMock(return_value=self.data['response'])
         self.scraper.run()
 
-        self.scraper.publish.assert_called_once_with(self.data['incident'])
+        self.scraper.publish.assert_called_once_with(self.data['incidents'])
 
     @patch('requests.get', return_value=MagicMock(autospec=True))
     def test_publish_no_incidents(self, post_mock):
@@ -164,15 +166,54 @@ class LUPowerOutagesTest(unittest.TestCase):
 
         size = len(incidents)
 
-        self.assertEqual(size, 3)
+        self.assertEqual(size, 1)
 
     def test_get_incident(self):
-        result = self.scraper.get_incident(self.data['raw_incident'])
+        result = self.data['raw_incident']
+        result = self.scraper.get_incident(result)
 
         incident_id = result['id']
         incident = result['incident']
         created_at = result['created_at']
+        longitude = result['longitude']
+        latitude = result['latitude']
+        consumers_affected = result['consumers_affected']
 
-        self.assertEqual(incident_id, '8680ba2f26da0213cdd3fa54eda9846a')
+        self.assertEqual(incident_id, '5501cc261b79ef4535eb70bf504d6e7f')
         self.assertEqual(incident, 'Power Outage')
-        self.assertEqual(created_at, 1471567800.0)
+        self.assertEqual(created_at, 1471567800)
+        self.assertEqual(longitude, '-84.0379808698967')
+        self.assertEqual(latitude, '36.3322327488042')
+        self.assertEqual(consumers_affected, '14')
+
+    def test_get_url(self):
+        url = self.scraper.get_url()
+
+        self.assertEqual(url, 'http://www.outageentry.com/CustomerFacingAppJQM/ajaxShellOut.php')
+
+    def test_get_params(self):
+        params = self.scraper.get_params()
+
+        param_1 = params['target']
+        param_2 = params['action']
+        param_3 = params['serviceIndex']
+        param_4 = params['url']
+        param_5 = params['']
+        param_6 = params['_']
+
+        self.assertEqual(param_1, 'device_markers')
+        self.assertEqual(param_2, 'get')
+        self.assertEqual(param_3, 1)
+        self.assertEqual(param_4, '10.58.27.9')
+        self.assertEqual(param_5, '3A80')
+        self.assertEqual(param_6, 1471567800)
+
+    def test_is_valid_incident(self):
+        valid_affected_raw_incident = {'consumers_affected': '20'}
+        invalid_affected_raw_incident = {'consumers_affected': '5'}
+
+        is_valid_affected = self.scraper.is_valid_incident(valid_affected_raw_incident)
+        is_invalid_affected = self.scraper.is_valid_incident(invalid_affected_raw_incident)
+
+        self.assertTrue(is_valid_affected)
+        self.assertFalse(is_invalid_affected)
